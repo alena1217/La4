1526f9e59c720c933fe4b79638bfb9c79e6f49d8
alena1217 <alenafilatova546@gmail.com>
2025-05-29 18:50:46 +0300

    Create 13.py

---
+++ 13.py
@@ -0,0 +1,8 @@
+class Employee:
+    def __init__(self, name, salary, age):
+        self.__name = name    
+        self.__salary = salary  
+        self.__age = age 
+
+    def get_employee_data(self):
+        return f"Name: {self.__name}, Salary: {self.__salary}, Age: {self.__age}"
